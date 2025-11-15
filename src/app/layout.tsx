9437e9e556ef44affa16dfc5a82a9ac8f163f0ba
Sharavana07 <Sharavanaragav@gmail.com>
2025-11-16 02:01:19 +0530

    Pushing layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import "./globals.css";
 import Navbar from "../app/components/Navbar";
+import React from "react"; // needed for React.ReactNode type
 
 export const metadata = {
   title: "Wefetch",
   description: "Turning Data Points into Sustainable Actions",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className="bg-white">
